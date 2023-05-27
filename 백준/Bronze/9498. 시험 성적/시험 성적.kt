fe816d51c4fa1bb0ffba833b246d810945729f7e
kska01 <81162740+kska01@users.noreply.github.com>
2023-05-27 21:31:14 +0900

    [Bronze V] Title: 시험 성적, Time: 88 ms, Memory: 12360 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.kt
@@ -0,0 +1,9 @@
+fun main() {
+    when (readln().toInt()) {
+        in 90..100 -> println("A")
+        in 80..89 -> println("B")
+        in 70..79 -> println("C")
+        in 60..69 -> println("D")
+        else -> println("F")
+    }
+}
