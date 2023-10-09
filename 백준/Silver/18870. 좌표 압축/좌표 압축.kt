cca0488241e96090e5a7766ceec46a196dfc1d2b
kska01 <81162740+kska01@users.noreply.github.com>
2023-10-09 22:35:00 +0900

    [Silver II] Title: 좌표 압축, Time: 3056 ms, Memory: 369672 KB -BaekjoonHub

---
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.kt
@@ -0,0 +1,19 @@
+val br = System.`in`.bufferedReader()
+
+fun main() =with(System.out.bufferedWriter()){
+
+    val n = br.readLine().toInt()
+    var arr = br.readLine().split(' ').map{it.toInt()}
+    val sortedArr = arr.distinct().sorted()
+    val hashMap = HashMap<Int,Int>()
+    var idx=0
+    sortedArr.forEach {
+        hashMap[it]=idx++
+    }
+
+    for(i in 0 until n){
+        write("${hashMap.get(arr[i])} ")
+    }
+
+    close()
+}
